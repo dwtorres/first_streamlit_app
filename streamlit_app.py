fa68d4c7f42d8a1ee7cac32d117b316e2c7537ab
David Torres <dwtorres@users.noreply.github.com>
2023-08-01 22:08:08 -0500

    Update streamlit_app.py
    Added JSON call

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import pandas
 import streamlit
 
 streamlit.title("My Parents New Healthy Diner")
-streamlit.header('Breakfast Menu')
+streamlit.header('Fruityvice Fruit Advice!')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
@@ -21,4 +21,4 @@ streamlit.dataframe(fruits_to_show)
 import requests
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
